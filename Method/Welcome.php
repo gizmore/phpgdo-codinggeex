ce921f0f7997e732692be4dac0bcbd0539686ba3
gizmore <gizmore@wechall.net>
2023-03-27 23:25:27 +0200

    '7.0.3-r1'

--- Method/Welcome.php
+++ Method/Welcome.php
@@ -3,10 +3,14 @@ namespace GDO\Codinggeex\Method;
 
 use GDO\Core\GDT;
 use GDO\Core\GDT_Version;
+use GDO\Core\WithFileCache;
 use GDO\UI\MethodPage;
+use GDO\User\GDO_User;
 
 final class Welcome extends MethodPage
 {
+
+	use WithFileCache;
 
 	public function execute(): GDT
 	{
